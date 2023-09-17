f8df1bd6fdaab3aa52ecd8702a5b1fd967fff59d
drumwell <drumwell@gmail.com>
2023-09-17 14:39:37 -0400

    Updates to allow the model to be trained by Michelle Kuo's dissertation

--- llm/extract_text.py
+++ llm/extract_text.py
@@ -18,8 +18,8 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
-def extract(url: str, start_page: int, end_page: int, 
-            header_height: int, footer_height: int, 
+def extract(url: str, start_page: int, end_page: int,
+            header_height: int, footer_height: int,
             extraction_path: Path) -> None:
     """Extract text from a PDF url.
     Pages are exported in a jsonl file.
@@ -35,7 +35,8 @@ def extract(url: str, start_page: int, end_page: int,
     response = requests.get(url)
     content = io.BytesIO(response.content)
     with pdfplumber.open(content) as pdf:
-        pages = extract_text_from_pdf(pdf, start_page, end_page, header_height, footer_height)
+        pages = extract_text_from_pdf(
+            pdf, start_page, end_page, header_height, footer_height)
     LOGGER.info(f'Finished extracting texts from {url}')
     to_jsonl(pages=pages, path=extraction_path)
 
@@ -44,7 +45,7 @@ def to_jsonl(pages: Iterator[Tuple[int, str]], path: Path) -> None:
     LOGGER.info(f'Start writing to {path}')
     # We append text to the existing file with "a" mode (append)
     with open(path, 'a') as f:
-        for page_number, text in tqdm(pages):    
+        for page_number, text in tqdm(pages):
             dict_page = {page_number: text}
             json.dump(dict_page, f)
             f.write('\n')
@@ -52,25 +53,27 @@ def to_jsonl(pages: Iterator[Tuple[int, str]], path: Path) -> None:
 
 
 def extract_cropped_text_from_page(page: Page, header_height: int, footer_height: int) -> str:
-    bbox = (0, header_height, page.width, footer_height) # Top-left corner, bottom-right corner
-    text = page.crop(bbox=bbox).extract_text()
+    # Top-left corner, bottom-right corner
+    bbox = (0, header_height, page.width, footer_height)
+    # text = page.crop(bbox=bbox).extract_text()
+    text = page.extract_text()
     return text
 
 
-def extract_text_from_pdf(pdf: PDF, start_page: int, end_page: int, 
+def extract_text_from_pdf(pdf: PDF, start_page: int, end_page: int,
                           header_height: int, footer_height: int) -> Generator[Tuple[int, str], None, None]:
     for page in tqdm(pdf.pages):
         if page.page_number >= start_page and page.page_number <= end_page:
-            yield page.page_number, extract_cropped_text_from_page(page=page, header_height=header_height, 
+            yield page.page_number, extract_cropped_text_from_page(page=page, header_height=header_height,
                                                                    footer_height=footer_height)
             # By default, pdfplumber keeps in cache to avoid to reprocess the same page, leading to memory issues.
             page.flush_cache()
 
 
 if __name__ == "__main__":
-    extract(url=config.url, 
-            start_page=config.start_page, 
-            end_page=config.end_page, 
-            header_height=config.header_height, 
+    extract(url=config.url,
+            start_page=config.start_page,
+            end_page=config.end_page,
+            header_height=config.header_height,
             footer_height=config.footer_height,
             extraction_path=config.extraction_path)
--- llm/prepare_dataset.py
+++ llm/prepare_dataset.py
@@ -6,28 +6,32 @@ from typing import Callable, Mapping
 from datasets import Dataset
 from transformers import AutoTokenizer, PreTrainedTokenizer
 
-from llm import config
+import config
 
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def prepare_dataset(dataset_path: Path, min_length: int, context_length: int, 
-                    test_size: float, shuffle: bool, hf_repo: str) -> None:
+def prepare_dataset(dataset_path: Path, min_length: int, context_length: int,
+                    test_size: float, shuffle: bool, hf_repo: str, hf_hub: str) -> None:
     """Prepare dataset for training and push it to the hub.
     """
-    tokenizer =  AutoTokenizer.from_pretrained(config.model_name)
+    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
     LOGGER.info(f'Start preparing dataset from {dataset_path}')
-    text = preprocess_data(dataset_path=dataset_path, min_length=min_length, tokenizer=tokenizer)
+    text = preprocess_data(dataset_path=dataset_path,
+                           min_length=min_length, tokenizer=tokenizer)
     dataset = Dataset.from_dict({'text': [text]})
     # We push the extracted book publicly
-    dataset.push_to_hub("JeremyArancio/lotr-book")
+    dataset.push_to_hub(hf_hub)
     tokenized_dataset = dataset.map(tokenize, batched=True, fn_kwargs={'tokenizer': tokenizer, 'context_length': context_length},
-                                         remove_columns=dataset.column_names)
-    LOGGER.info(f'The tokenized dataset is composed of {tokenized_dataset.num_rows} elements, each one composed of {context_length} tokens.')
-    tokenized_dataset_dict = tokenized_dataset.train_test_split(test_size=test_size, shuffle=shuffle)
-    LOGGER.info(f'The training dataset is composed of {tokenized_dataset_dict["train"].num_rows} elements, the test dataset is composed of {tokenized_dataset_dict["test"].num_rows} elements.')
+                                    remove_columns=dataset.column_names)
+    LOGGER.info(
+        f'The tokenized dataset is composed of {tokenized_dataset.num_rows} elements, each one composed of {context_length} tokens.')
+    tokenized_dataset_dict = tokenized_dataset.train_test_split(
+        test_size=test_size, shuffle=shuffle)
+    LOGGER.info(
+        f'The training dataset is composed of {tokenized_dataset_dict["train"].num_rows} elements, the test dataset is composed of {tokenized_dataset_dict["test"].num_rows} elements.')
     tokenized_dataset_dict.push_to_hub(hf_repo)
     LOGGER.info(f'Preparing dataset finished.')
 
@@ -61,11 +65,11 @@ def preprocess_text(text: str) -> str:
 
 
 def tokenize(element: Mapping, tokenizer: Callable, context_length: int) -> str:
-    inputs = tokenizer(element['text'], truncation=True, return_overflowing_tokens=True, 
+    inputs = tokenizer(element['text'], truncation=True, return_overflowing_tokens=True,
                        return_length=True, max_length=context_length)
     inputs_batch = []
     for length, input_ids in zip(inputs['length'], inputs['input_ids']):
-        if length == context_length: # We drop the last input_ids that are shorter than max_length
+        if length == context_length:  # We drop the last input_ids that are shorter than max_length
             inputs_batch.append(input_ids)
     return {"input_ids": inputs_batch}
 
@@ -73,10 +77,11 @@ def tokenize(element: Mapping, tokenizer: Callable, context_length: int) -> str:
 if __name__ == '__main__':
 
     prepare_dataset(
-        dataset_path=config.extraction_path, 
+        dataset_path=config.extraction_path,
         min_length=config.min_length,
         context_length=config.context_length,
         test_size=config.test_size,
         shuffle=config.shuffle,
-        hf_repo=config.hf_repo
+        hf_repo=config.hf_repo,
+        hf_hub=config.hf_hub
     )
